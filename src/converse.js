81c460744d05834fef536d4aacb31f598aa58064
WyvernDrexx <lilaprashadsharma@gmail.com>
2020-10-03 19:11:04 +0530

    readme.md add

--- src/converse.js
+++ src/converse.js
@@ -1,12 +1,11 @@
 const converse = (messageObj) => {
   const joinText = messageObj.content.toLowerCase().split(" ").join("");
   const messageContent = messageObj.content.toLowerCase().trim();
-  // if(messageContent.includes("")){
-  //   messageObj.reply("The word is not allowed!");
-  //   messageObj.delete();
-  //   return;
-  // }
-
+  if(messageContent.includes("fuck")){
+    messageObj.reply("The word is not allowed!");
+    messageObj.delete();
+    return;
+  }
   console.log(messageObj.mentions.users.keys().length)
 
   if(messageContent === "hi"){
